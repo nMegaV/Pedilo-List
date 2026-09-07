ff7c25d49f0196e875124e4e4f2de4ff384a1ccf
nMegaV <edgyboyvi@gmail.com>
2026-09-07 15:59:53 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,30 +98,30 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3>Requisitos</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Tenes que haberte pasado el nivel sin usar hacks
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Manda un video pasandote el nivel al discord y ahi se ve si lo ponemos en la lista
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        No hace falta que tenga audio de clicks, con un indicador de clicks en el nivel es suficiente
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Que se vean mas intentos que solo en el que te pasas el nivel
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        El video tiene que mostrar como llegas hasta el final del nivel
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        No te aproveches de bugs o secret ways
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        No uses algo como un modo facil para pasarte el nivel
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Y bueno eso chavaleria, a disfrutar la PEDILO list
                     </p>
                 </div>
             </div>
