1d16983e04481547ed1cc8ffe0513047aba6c701
nMegaV <edgyboyvi@gmail.com>
2026-09-08 03:10:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import routes from './routes.js';
+
+export const store = Vue.reactive({
+    dark: true,
+});
+
+const app = Vue.createApp({
+    data: () => ({ store }),
+});
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+app.use(router);
+
+app.mount('#app');
